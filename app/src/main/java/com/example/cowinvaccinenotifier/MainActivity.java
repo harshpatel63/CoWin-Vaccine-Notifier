f57701d01f7656cd9e25cff7598cd3e16ef42c7f
Harsh <patelharsh0603@gmail.com>
2021-07-19 22:36:33 +0530

    Implemented the custom bottom navigation bar

--- app/src/main/java/com/example/cowinvaccinenotifier/MainActivity.java
+++ app/src/main/java/com/example/cowinvaccinenotifier/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cowinvaccinenotifier;
 
 import android.os.Bundle;
+import android.widget.PopupMenu;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,13 +24,16 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_statistics, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+        PopupMenu popupMenu = new PopupMenu(this, null);
+        popupMenu.inflate(R.menu.bottom_nav_menu);
+        binding.navView.setupWithNavController(popupMenu.getMenu(), navController);
     }
 
 }
